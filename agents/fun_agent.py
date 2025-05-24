ce63b11e597f41096200ae01822911118084f249
jenny930708 <930708jenny@gmail.com>
2025-05-24 16:37:05 +0800

    Update fun_agent.py

--- agents/fun_agent.py
+++ agents/fun_agent.py
@@ -1,15 +1,21 @@
+from linebot.models import TextSendMessage, ImageSendMessage
+
 def handle_fun(user_message):
     if "梗圖" in user_message:
-        return "這張貓貓圖是不是很療癒？https://i.imgur.com/Jt6plWY.jpeg"
+        return ImageSendMessage(
+            original_content_url="https://i.imgur.com/Jt6plWY.jpeg",
+            preview_image_url="https://i.imgur.com/Jt6plWY.jpeg"
+        )
     elif "音樂" in user_message:
-        return "這首歌也許能振奮你的心情：https://www.youtube.com/watch?v=ZbZSe6N_BXs"
+        return TextSendMessage(text="這首歌也許能振奮你的心情：https://www.youtube.com/watch?v=ZbZSe6N_BXs")
     elif "影片" in user_message:
-        return "這支短影片讓你笑一笑：https://www.youtube.com/shorts/abc123xyz"
+        return TextSendMessage(text="這支短影片讓你笑一笑：https://www.youtube.com/shorts/abc123xyz")
     return None
+
 def handle_music_request(user_message):
     if "周杰倫" in user_message:
-        return "這是周杰倫的經典歌曲，希望你喜歡～ https://www.youtube.com/watch?v=2jD5V8YVhJM"
+        return TextSendMessage(text="這是周杰倫的經典歌曲，希望你喜歡～ https://www.youtube.com/watch?v=2jD5V8YVhJM")
     elif "別的" in user_message or "換一首" in user_message:
-        return "試試這首新歌看看，也許會讓你感覺更放鬆：https://www.youtube.com/watch?v=UfcAVejslrU"
+        return TextSendMessage(text="試試這首新歌看看，也許會讓你感覺更放鬆：https://www.youtube.com/watch?v=UfcAVejslrU")
     else:
-        return "這首歌也許能振奮你的心情：https://www.youtube.com/watch?v=ZbZSe6N_BXs"
+        return TextSendMessage(text="這首歌也許能振奮你的心情：https://www.youtube.com/watch?v=ZbZSe6N_BXs")
